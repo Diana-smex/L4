5c8807251a84f448a62a1861d08923c01735a947
Мирхайдарова Диана <lamyrr@bk.ru>
2025-11-22 12:59:34 +0500

    Добавлен класс Rectangle для прямоугольников.

---
+++ rectangle.cpp
@@ -0,0 +1,65 @@
+﻿#include "rectangle.h"
+
+// Конструктор прямоугольника
+Rectangle::Rectangle(const QPoint& center, int width, int height)
+    : Shape(center), m_width(width), m_height(height)
+{
+}
+
+// Рисование прямоугольника
+void Rectangle::draw(QPainter& painter)
+{
+    // Установка цвета в зависимости от выделения
+    if (m_selected) {
+        painter.setPen(QPen(Qt::red, 2));
+    }
+    else {
+        painter.setPen(QPen(m_color, 2));
+    }
+
+    // Заливка фигуры
+    painter.setBrush(QBrush(m_color, Qt::Dense4Pattern));
+    // Создание прямоугольника с центром в заданной точке
+    QRect rect(m_center.x() - m_width / 2, m_center.y() - m_height / 2, m_width, m_height);
+    painter.drawRect(rect);
+}
+
+// Проверка попадания точки в прямоугольник
+bool Rectangle::contains(const QPoint& point)
+{
+    QRect rect(m_center.x() - m_width / 2, m_center.y() - m_height / 2, m_width, m_height);
+    return rect.contains(point);
+}
+
+// Получение ограничивающего прямоугольника
+QRect Rectangle::getBoundingRect()
+{
+    return QRect(m_center.x() - m_width / 2, m_center.y() - m_height / 2, m_width, m_height);
+}
+
+// Изменение размера прямоугольника
+void Rectangle::resize(int delta)
+{
+    m_width += delta;
+    m_height += delta;
+
+    // Минимальные размеры
+    if (m_width < 20) {
+        m_width = 20;
+    }
+    if (m_height < 15) {
+        m_height = 15;
+    }
+}
+
+// Получение ширины прямоугольника
+int Rectangle::getWidth() const
+{
+    return m_width;
+}
+
+// Получение высоты прямоугольника
+int Rectangle::getHeight() const
+{
+    return m_height;
+}
---
+++ rectangle.h
@@ -0,0 +1,26 @@
+﻿#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include "shape.h"
+
+// Класс прямоугольника, наследуется от Shape
+class Rectangle : public Shape
+{
+private:
+    int m_width;   // Ширина прямоугольника
+    int m_height;  // Высота прямоугольника
+
+public:
+    Rectangle(const QPoint& center, int width = 60, int height = 40);
+
+    // Реализация виртуальных методов
+    void draw(QPainter& painter) override;
+    bool contains(const QPoint& point) override;
+    QRect getBoundingRect() override;
+    void resize(int delta) override;
+
+    int getWidth() const;   // Получение ширины
+    int getHeight() const;  // Получение высоты
+};
+
+#endif // RECTANGLE_H
